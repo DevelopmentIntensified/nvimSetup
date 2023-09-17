96f29b181f9d3d335e3cec9d9d27404adfbebdbd
MIRP <developmentIntensified@gmail.com>
2023-09-17 09:42:42 -0400

    message

--- lua/thisBetterWork/lsp/servers.lua
+++ lua/thisBetterWork/lsp/servers.lua
@@ -4,8 +4,8 @@ return {
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
+  cssls = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
--- lua/thisBetterWork/plugins/theme.lua
+++ lua/thisBetterWork/plugins/theme.lua
@@ -3,15 +3,38 @@
 --   lazy = false,
 --   priority = 1000,
 --   config = function()
---     vim.cmd.colorscheme = 'transparent'
+--     vim.cmd.colorscheme 'monokai-nightasty'
 --   end
 -- }
+--
+-- return {
+--   -- Theme inspired by Atom
+--   'navarasu/onedark.nvim',
+--   priority = 1000,
+--   config = function()
+    -- vim.cmd.colorscheme 'onedark'
+--   end,
+-- }
 
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {
+--     style = "moon",
+--     hide_inactive_statusline = true,
+--   },
+--   config = function()
+--     vim.cmd.colorscheme 'tokyonight'
+--   end,
+-- }
+--
 return {
-  -- Theme inspired by Atom
-  'navarasu/onedark.nvim',
+  'uloco/bluloco.nvim',
+  lazy = false,
   priority = 1000,
+  dependencies = { 'rktjmp/lush.nvim' },
   config = function()
-    vim.cmd.colorscheme 'onedark'
+    vim.cmd.colorscheme 'bluloco'
   end,
 }
