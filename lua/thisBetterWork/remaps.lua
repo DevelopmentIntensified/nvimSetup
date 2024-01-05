93fdea05b9234b7d0e45825720b9ade41ac27be6
mpetke <mpetke@liberty.edu>
2024-01-05 12:09:29 -0500

    some keymap fixes

--- lua/thisBetterWork/remaps.lua
+++ lua/thisBetterWork/remaps.lua
@@ -59,3 +59,20 @@ else
     { desc = "Prime: jump to config file" });
 end
 -- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+-- I caved...
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+--navigation between windows
+vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l")
+vim.keymap.set("i", "<A-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("i", "<A-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("i", "<A-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("i", "<A-l>", "<C-\\><C-N><C-w>l")
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
